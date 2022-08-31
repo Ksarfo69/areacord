88924a3d946460f34c47a4ce3c561afdb7771d15
ksarfo69 <ksarfo69@gmail.com>
2022-08-31 17:32:06 +0000

    feat: create index page

--- components/navigation/Sidebar/Sidebar.tsx
+++ components/navigation/Sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar: React.FC<ISidebar> = () => {
     <div className="flex flex-col fixed h-screen md:w-1/5 lg:w-1/5 bg-gradient-to-t from-purple-200 to-purple-600">
       <div className="flex flex-col items-center justify-center w-full h-1/2 text-white">
         <div className="relative w-1/4 h-1/4">
-          <Image src="favicon.ico" alt="" layout="fill" />
+          <Image src="/favicon.ico" alt="" layout="fill" />
         </div>
         <div className="text-4xl md:text-3xl">Areacord</div>
         <div className="3xl text-center">A community chat app</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,37 +4,37 @@ import MessageBox from '../components/utils/MessageBox/MessageBox';
 
 const messages = Array(
   {
-    image: '/public/favicon.ico',
+    image: '/favicon.ico',
     name: 'Kwame',
     time: '12:48 PM',
     message: 'This is message one',
   },
   {
-    image: '/public/favicon.ico',
+    image: '/favicon.ico',
     name: 'Adu',
     time: '12:48 PM',
     message: 'This is message two',
   },
   {
-    image: '/public/favicon.ico',
+    image: '/favicon.ico',
     name: 'Poku',
     time: '12:48 PM',
     message: 'This is message three',
   },
   {
-    image: '/public/favicon.ico',
+    image: '/favicon.ico',
     name: 'Sarfo',
     time: '12:48 PM',
     message: 'This is message four',
   },
   {
-    image: '/public/favicon.ico',
+    image: '/favicon.ico',
     name: 'Sarfo',
     time: '12:48 PM',
     message: 'This is message four',
   },
   {
-    image: '/public/favicon.ico',
+    image: '/favicon.ico',
     name: 'Sarfo',
     time: '12:48 PM',
     message: 'This is message four',
