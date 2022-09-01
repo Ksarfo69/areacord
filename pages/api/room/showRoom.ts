c47f0f54256a3d48dfd0be306c237c9bebd1ae1d
ksarfo69 <ksarfo69@gmail.com>
2022-09-01 13:12:30 +0000

    feat: create show chat room api

--- pages/api/room/showRoom.ts
+++ pages/api/room/showRoom.ts
@@ -1,12 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { connectPG, dbConn } from '../../../config/dbConn';
 
-type Data = {
-  name: string;
-};
+type Data = {};
 
-export default function handler(
+interface IResponse {
+  rows: Array<{
+    message_id: number;
+    room_id: number;
+    user_id: number;
+    message: string;
+    time: string;
+  }>;
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  await connectPG();
+  const roomID: number = req.body.room_id;
+
+  const query: string = 'SELECT * FROM messages WHERE room_id = $1';
+  await dbConn.query(query, [roomID], (err: Error, results: IResponse) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    return res.status(200).send(results.rows);
+  });
 }
